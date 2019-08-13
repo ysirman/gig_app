688d64946be034ce9614c0ae8553622fb66341de
ysirman <24975537+ysirman@users.noreply.github.com>
2019-08-13 20:32:26 +0900

    ユーザー一覧ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'users/show'
   resources :events
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show]
   root "home#index"
 end
