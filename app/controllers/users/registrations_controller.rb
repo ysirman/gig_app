e4a7cca73af2482ad8080da804e3d0da5d7272c7
ysirman <24975537+ysirman@users.noreply.github.com>
2019-10-22 13:03:12 +0900

    パスワード無しでユーザー更新可能にする

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,4 +4,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
     hash[:uid] = User.create_unique_string
     super
   end
+
+  protected
+    def update_resource(resource, params)
+      resource.update_without_password(params)
+    end
+
+    def after_update_path_for(resource)
+      user_path(current_user.id)
+    end
 end
