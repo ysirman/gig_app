2a03cc5825f2b0644d374d5fcb5886462fe286bd
ysirman <24975537+ysirman@users.noreply.github.com>
2019-10-31 23:16:07 +0900

    コメント投稿時にエラーメッセージを表示する

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class Comment < ApplicationRecord
-  validates :description, presence: true
-
   belongs_to :user
   belongs_to :event
   validates :user_id, presence: true
   validates :event_id, presence: true
+  validates :description, presence: true
 
   # kaminariの設定
   paginates_per 30
